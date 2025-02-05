a4ca2bc3955ab8f8f0ea0cb7a94ecf86980fba57
Tobiasz Izydor <tobiasz.izydor02@gmail.com>
2025-02-05 20:48:16 +0100

    Updated UI  + added sidebar to all actions

---
+++ TiErp.MVC/Views/Shared/_LoginPartialSideBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TiErp.MVC.Views.Shared
+{
+    public class _LoginPartialSideBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- TiErp.MVC/wwwroot/js/site.js
+++ TiErp.MVC/wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    $(".toggle-btn").click(function () {
+        $(".link-text").toggleClass("hidden");
+        $("aside .toggle-size").each(function () {
+            var element = $(this);
+            if (element.width() === 24 && element.height() === 24) {
+                element.css({
+                    "width": "48px",
+                    "line-height": "48px",
+                    "height": "48px"
+                });
+            } else {
+                element.css({
+                    "width": "24px",
+                    "line-height": "24px",
+                    "height": "24px"
+                });
+            }
+        });
+        $("aside").toggleClass("p-3").toggleClass("p-1");
+        $(".rotate").toggleClass("rotate-180");
+        $("aside .navbar-nav").toggleClass("mx-auto");
+        $("aside li .collapse").removeClass("show");
+        $("aside .nav").each(function () {
+            var element = $(this);
+            if (element.width() === 250) {
+                element.css({
+                    "width": "77.6px"
+                });
+            } else {
+                element.css({
+                    "width": "250px"
+                });
+            }
+        });
+    });
+});
